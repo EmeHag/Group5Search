b49d4ebc9a07aa6a21c2c37fe31e3ed0f2efcf50
Maria Berg <bermak-2@student.ltu.se>
2023-04-26 18:33:17 +0200

    Mergestrul

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -22,24 +22,52 @@ public class Main {
 
         var credentials = readCredentialsFromFile();
 
-        // Create ChromeOptions instance and add the --disable-notifications argument
+        var driver = openPageInChrome("https://sv-se.facebook.com/");
+
+        //Calls the logIn method that is used to log in to Facebook
+        logIn(credentials, driver);
+
+        sleepForSeconds(3);
+
+        //Calling the search method
+        search("Java", driver);
+
+        //Calling the sleep method
+        sleepForSeconds(4);
+
+        // Close the browser
+        driver.quit();
+    }
+
+    //Reading the facebook credentails from the json file
+    private static Credentials readCredentialsFromFile() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(new File("src/main/resources/Facebook.json"), Credentials.class);
+        } catch (IOException e) {
+            logger.error("Kunde inte öppna filen", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Open the choosen page in Chrome Webdriver
+    private static WebDriver openPageInChrome(String url) {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications");
         options.addArguments("--start-maximized");
 
-        // Set the path to the ChromeDriver executable
+        //in second argument, insert path to executable chromedriver file
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\maria\\.cache\\selenium\\chromedriver\\win32\\112.0.5615.49\\chromedriver.exe");
-
-        // Launch the Chrome browser
         WebDriver driver = new ChromeDriver(options);
-
-        // Navigate to the Facebook login page
-        driver.get("https://sv-se.facebook.com/");
-
-        // Accept only necessary cookies
+        driver.get(url);
         WebElement button = driver.findElement(By.xpath("//button[text()='Tillåt endast nödvändiga cookies']"));
         button.click();
+        return driver;
+    }
 
+
+    //Logging in using the facebook credentials from the json file
+    private static void logIn(Credentials credentials, WebDriver driver) {
         WebElement emailInput = driver.findElement(By.id("email"));
         emailInput.sendKeys(credentials.email());
 
@@ -48,36 +76,27 @@ public class Main {
 
         WebElement loginButton = driver.findElement(By.name("login"));
         loginButton.click();
-
-        sleepForSeconds(3);
-
-        // Click on the search bar, write "Java" and click enter to search
-        var sok = driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div[2]/div[3]/div/div/div/div/div/label"));
-        sok.click();
-        sok.sendKeys("Java");
-        sok.sendKeys(Keys.ENTER);
-
-        sleepForSeconds(4);
-
-        // Close the browser
-        driver.quit();
     }
 
+    //Searching based on the input argument
+    private static void search(String searchPhrase, WebDriver driver) {
+        try {
+            var search = driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div[2]/div[3]/div/div/div/div/div/label"));
+            search.click();
+            search.sendKeys(searchPhrase);
+            search.sendKeys(Keys.ENTER);
+        } catch (Exception e) {
+            logger.error("Sökning kunde inte genomföras korrekt ", e);
+            e.printStackTrace();
+        }
+    }
+
+    //Pausing the program for chosen amount of seconds
     private static void sleepForSeconds(int s) {
         try {
             Thread.sleep(Duration.ofSeconds(s));
         } catch (InterruptedException e) {
             logger.error("Kunde inte pausa", e);
-        }
-    }
-
-    private static Credentials readCredentialsFromFile() {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            return mapper.readValue(new File("src/main/resources/Facebook.json"), Credentials.class);
-        } catch (IOException e) {
-            logger.error("Kunde inte öppna filen", e);
-            throw new RuntimeException(e);
         }
     }
 }
